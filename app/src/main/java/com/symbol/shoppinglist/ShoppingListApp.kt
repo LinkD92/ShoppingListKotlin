7701d109342271ec59dc5286bd13331d4dfebc4c
LinkD92 <46903281+LinkD92@users.noreply.github.com>
2022-10-05 09:42:11 +0200

    State moved to VM for textField

--- app/src/main/java/com/symbol/shoppinglist/ShoppingListApp.kt
+++ app/src/main/java/com/symbol/shoppinglist/ShoppingListApp.kt
@@ -1,21 +1,11 @@
 package com.symbol.shoppinglist
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.symbol.shoppinglist.addProduct.AddProductView
-import com.symbol.shoppinglist.product.DisplayProducts
 import com.symbol.shoppinglist.ui.AppFab
 import com.symbol.shoppinglist.ui.AppTopBar
 import com.symbol.shoppinglist.ui.NavigationGraph
