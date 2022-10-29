3269ba664943bc08a9b99ddb22e17f678d5749a1
LinkD92 <46903281+LinkD92@users.noreply.github.com>
2022-10-29 12:03:22 +0200

    Products use cases tests

---
+++ app/src/test/java/com/symbol/shoppinglist/feature_product/domain/use_case/GetCategoryProductsTest.kt
@@ -0,0 +1,12 @@
+package com.symbol.shoppinglist.feature_product.domain.use_case
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class GetCategoryProductsTest {
+
+    @Before
+    fun setUp() {
+    }
+}
---
+++ app/src/test/java/com/symbol/shoppinglist/feature_product/domain/use_case/GetProductsTest.kt
@@ -0,0 +1,5 @@
+package com.symbol.shoppinglist.feature_product.domain.use_case
+
+import org.junit.Assert.*
+
+class GetProductsTest
