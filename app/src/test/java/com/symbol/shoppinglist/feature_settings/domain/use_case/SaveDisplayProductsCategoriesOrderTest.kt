fc6ae79a2b2c24e8fc7cc16a3f3cce85cd5d0f45
LinkD92 <46903281+LinkD92@users.noreply.github.com>
2022-11-07 21:02:41 +0100

    Settings view changed slightly. Test structure

---
+++ app/src/test/java/com/symbol/shoppinglist/feature_settings/domain/use_case/SaveDisplayProductsCategoriesOrderTest.kt
@@ -0,0 +1,12 @@
+package com.symbol.shoppinglist.feature_settings.domain.use_case
+
+import org.junit.Assert.*
+
+import org.junit.Before
+
+class SaveDisplayProductsCategoriesOrderTest {
+
+    @Before
+    fun setUp() {
+    }
+}
