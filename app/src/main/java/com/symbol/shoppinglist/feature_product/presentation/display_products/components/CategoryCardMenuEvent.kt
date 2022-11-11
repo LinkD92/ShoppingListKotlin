ca92b7fb536e7b368d43987350e470e01de2fe35
LinkD92 <46903281+LinkD92@users.noreply.github.com>
2022-11-11 17:33:51 +0100

    Added missing class

--- app/src/main/java/com/symbol/shoppinglist/feature_product/presentation/display_products/components/CategoryCardMenuEvent.kt
+++ app/src/main/java/com/symbol/shoppinglist/feature_product/presentation/display_products/components/CategoryCardMenuEvent.kt
@@ -1,4 +1,7 @@
 package com.symbol.shoppinglist.feature_product.presentation.display_products.components
 
-class CategoryCardMenuEvent {
+import com.symbol.shoppinglist.feature_product.domain.model.Product
+
+sealed class CategoryCardMenuEvent {
+    data class CheckUncheckAll(val products: List<Product>): CategoryCardMenuEvent()
 }
